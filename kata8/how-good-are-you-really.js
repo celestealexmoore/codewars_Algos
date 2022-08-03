590d8df3da52adb5ae0e7aa0c0505d8a6816dff9
cMoore <celestealexmoore@gmail.com>
2022-08-03 16:08:47 -0500

    kata8 how good are you really?

---
+++ kata8/how-good-are-you-really.js
@@ -0,0 +1,32 @@
+/* 
+
+DESCRIPTION:
+There was a test in your class and you passed it. Congratulations!
+But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+Return True if you're better, else False!
+
+Note:
+Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array! 
+*/
+
+function betterThanAverage(classPoints, yourPoints) {
+  let classSum = yourPoints;
+  for (let i = 0; i < classPoints.length; i++) {
+    classSum += classPoints[i];
+  }
+  classPoints.length += 1;
+  let averageGrades = classSum / classPoints.length;
+  if (averageGrades < yourPoints) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+/* 
+Link to Kata:
+https://www.codewars.com/kata/5601409514fc93442500010b 
+*/
