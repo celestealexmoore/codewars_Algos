c3c7c0fc0594e333ed8b669fc525b9029542a1e3
cMoore <celestealexmoore@gmail.com>
2022-08-03 16:42:28 -0500

    kata8 - hello-world

---
+++ kata8/function-1-hello-world.js
@@ -0,0 +1,14 @@
+/* 
+Description:
+Make a simple function called greet that returns the most-famous "hello world!".
+*/
+
+
+function greet() {
+  return "hello world!";
+}
+
+/* 
+Link to the Kata:
+https://www.codewars.com/kata/523b4ff7adca849afe000035/train/javascript
+*/
